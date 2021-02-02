20d066fe6e6cd8d45f0568c042ad55e34f2680ac
Joaquinma Prueba <monterona@outlook.es>
2021-02-02 15:11:13 +0100

    Saludo ampliado

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,5 +4,6 @@
 public class HolaMundo {
    public static void main(String[] args) {
 	System.out.println("¡Hola mundo!");
+	System.out.println("Aquí estoy con Esperanza estudiando con ella");
    }
 }

17980a151be055dcb0c8cebeb49045a67cff79ee
Joaquinma Prueba <monterona@outlook.es>
2021-02-02 16:21:23 +0100

    Añadida comillas y colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,7 +3,7 @@
 */
 public class HolaMundo {
    public static void main(String[] args) {
-	String naranja ="\003[33m;
+	String naranja ="\003[33m";
         String azul = "\033[34m";
 
 	System.out.println("¡Hola mundo!");
